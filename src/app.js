e698a9b15b1505372e5ab2bee4cca85779ea7cbe
AbsolucionArtistica <122127365+AbsolucionArtistica@users.noreply.github.com>
2023-05-20 12:27:07 -0400

    terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var pronoun = ["the", "our"];
+  var adjetive = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+
+  for (
+    let pronounSelector = 0;
+    pronounSelector < pronoun.length;
+    pronounSelector++
+  ) {
+    for (
+      let adjetiveSelector = 0;
+      adjetiveSelector < adjetive.length;
+      adjetiveSelector++
+    ) {
+      for (let nounSelector = 0; nounSelector < noun.length; nounSelector++) {
+        console.log(
+          pronoun[pronounSelector] +
+            adjetive[adjetiveSelector] +
+            noun[nounSelector] +
+            ".com"
+        );
+      }
+    }
+  }
+};
 };
